ee971a2d8cd5116d79eb06b078d829d278ea54fa
Charles Korn <me@charleskorn.com>
2020-10-20 11:45:22 +1100

    Fix URL used to retrieve update information file.

--- src/worker.js
+++ src/worker.js
@@ -16,7 +16,7 @@ async function handleRequest(request) {
 
 async function getResponse(request) {
     if (request.url === 'https://updates.batect.dev/v1/latest') {
-        return await fetch(new Request('https://storage.cloud.google.com/batect-updates-prod-public/v1/latest.json'));
+        return await fetch(new Request('https://storage.googleapis.com/batect-updates-prod-public/v1/latest.json'));
     } else if (request.url === 'https://updates.batect.dev/ping') {
         return new Response('pong', { status: 200 });
     } else if (request.url === 'https://updates.batect.dev/') {
